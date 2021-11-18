9ea7b33f1802ebb43b4cbc4b64018f5c503e0865
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-11-19 00:34:04 +0600

    authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Home from './pages/Home/Home/Home';
+import PrivateRoute from './pages/Login/Login/PrivateRoute/PrivateRoute';
+import Services from './pages/Home/Services/Services';
+import Login from './pages/Login/Login/Login';
+import Register from './pages/Login/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+            <Route path='/home'>
+              <Home></Home>
+            </Route>
+            <PrivateRoute path='/services'>
+              <Services></Services>
+            </PrivateRoute>
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+            <Route path='/register'>
+              <Register></Register>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
