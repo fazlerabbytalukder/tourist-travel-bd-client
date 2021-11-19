7e53cf0f6de187e8b4ed48ff6952fb5e7e4d5ba3
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-11-19 21:51:08 +0600

    our pertners added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import Banner from '../Banner/Banner';
 import Footer from '../Footer/Footer';
+import OurPartners from '../OurPartners/OurPartners';
 import OurServices from '../OurServices/OurServices';
 
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Navigation></Navigation>
             <Banner></Banner>
             <OurServices></OurServices>
+            <OurPartners></OurPartners>
             <Footer></Footer>
         </div>
     );
